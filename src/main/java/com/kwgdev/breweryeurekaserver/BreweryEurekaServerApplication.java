56b201cd0fd35d5729b343b3531d5836afeaafb4
thrwwy216 <thrwwy216@tutanota.com>
2021-01-18 10:03:45 -0600

    init commit

---
+++ src/main/java/com/kwgdev/breweryeurekaserver/BreweryEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.kwgdev.breweryeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryEurekaServerApplication.class, args);
+    }
+
+}
